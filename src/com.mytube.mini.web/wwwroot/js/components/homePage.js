ab3c49f8041298b203d44c2ab275d37f39c9a395
antonmisiura <misiura.anton@gmail.com>
2017-04-09 16:05:22 +0300

    react components

---
+++ src/com.mytube.mini.web/wwwroot/js/components/homePage.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+var React = require("react");
+
+var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="jumbotron">
+                <h1>Mircotube</h1>
+                <p>This is react, react router, and flux</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
