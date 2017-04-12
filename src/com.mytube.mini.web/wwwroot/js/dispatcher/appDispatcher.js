eec099ad79815ed313b63d6a5f439133c291022b
antonmisiura <misiura.anton@gmail.com>
2017-04-13 02:26:48 +0300

    flux

---
+++ src/com.mytube.mini.web/wwwroot/js/dispatcher/appDispatcher.js
@@ -0,0 +1,13 @@
+﻿/*
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * AppDispatcher
+ *
+ * A singleton that operates as the central hub for application updates.
+ */
+
+var Dispatcher = require("flux").Dispatcher;
+
+module.exports = new Dispatcher();
+
